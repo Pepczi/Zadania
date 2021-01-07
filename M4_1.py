3278ca6e1713bf6caf34b1cf573d9940753d0fe0
Pepczi <67873453+Pepczi@users.noreply.github.com>
2021-01-07 18:20:17 +0100

    Update M4_1.py

--- M4_1.py
+++ M4_1.py
@@ -2,10 +2,13 @@
 Funkcja sprawdzająca czy podane słowo jest palindromem
 '''
 def is_palindrome(word):
-  print(word == word[::-1])
+  return word == word[::-1]
 
 word_given = input("Podaj słowo: ")
-is_palindrome(word_given)
+if is_palindrome(word_given):
+  print(f"Tak, {word_given} to palindrom")
+else:
+  print(f"{word_given} nie jest palindromem")
 
 
 
